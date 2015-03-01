30762be7451f65e8d2a00bef690d0e4d22df3333
Silend <solokhinii@gmail.com>
2015-03-01 23:45:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/silend/guesscountry/QuizFragment.java
@@ -0,0 +1,9 @@
+package com.example.silend.guesscountry;
+
+import android.app.Fragment;
+
+/**
+ * Created by silend on 01.03.2015.
+ */
+public class QuizFragment extends Fragment{
+}
---
+++ app/src/main/java/com/example/silend/guesscountry/SettingsFragment.java
@@ -0,0 +1,9 @@
+package com.example.silend.guesscountry;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by silend on 01.03.2015.
+ */
+public class SettingsFragment extends PreferenceFragment{
+}
